3cc05ce932f80d9ea774c369dcf4380bbf16196a
Sansko1337 <sanderknauff@hotmail.com>
2017-04-22 00:49:17 +0200

    Add LDAP authentication

--- src/main/java/nl/imine/radio/configuration/SpringSecurityConfiguration.java
+++ src/main/java/nl/imine/radio/configuration/SpringSecurityConfiguration.java
@@ -1,28 +1,66 @@
 package nl.imine.radio.configuration;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import java.util.Arrays;
 
-@Configuration
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.encoding.LdapShaPasswordEncoder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+
+@EnableWebSecurity
 public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Value("${ldap.url}")
+    private String ldapUrl;
+    @Value("${ldap.baseDn}")
+    private String ldapBaseDn;
+    @Value("${ldap.groupSearchBase}")
+    private String ldapGroupSearchBase;
+    @Value("${ldap.userDnPattern}")
+    private String ldapUserDnPattern;
+    @Value("${ldap.passwordAttribute}")
+    private String ldapPasswordAttribute;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                //MVC
-                .antMatchers("/").permitAll()
-                
-                .antMatchers("/edit/**").authenticated()
-                .antMatchers("/delete/**").authenticated()
 
-                //Radio API
-                .antMatchers(HttpMethod.DELETE, "/radio/**").authenticated()
-                .antMatchers(HttpMethod.PUT, "/radio/**").authenticated()
-                .antMatchers("/radio/**").permitAll();
+                    //MVC
+                    .antMatchers("/").permitAll()
 
+                    .antMatchers("/edit/**").fullyAuthenticated()
+                    .antMatchers("/delete/**").fullyAuthenticated()
+
+                    //Radio API
+                    .antMatchers(HttpMethod.DELETE, "/radio/**").fullyAuthenticated()
+                    .antMatchers(HttpMethod.PUT, "/radio/**").fullyAuthenticated()
+                    .antMatchers("/radio/**").permitAll()
+                    .and()
+                .formLogin();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .ldapAuthentication()
+                .userDnPatterns(ldapUserDnPattern)
+                .groupSearchBase(ldapGroupSearchBase)
+                .contextSource(contextSource())
+                .passwordCompare()
+                .passwordEncoder(new LdapShaPasswordEncoder())
+                .passwordAttribute(ldapPasswordAttribute);
+    }
+
+    @Bean
+    protected DefaultSpringSecurityContextSource contextSource() {
+        return new DefaultSpringSecurityContextSource(Arrays.asList(ldapUrl), ldapBaseDn);
     }
 
 }
